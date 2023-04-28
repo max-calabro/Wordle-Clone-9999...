f8f5cdac309afa0e4c06403a46d63524e79e10ce
Max Calabro <maxccalabro@gmail.com>
2023-04-28 16:37:53 -0400

    started game board

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ let solution = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(solution)
 
 const initGameBoard = () => {
+  //make "game-board-and-keyboard" flex
+  let gameBoardAndKeyboardDiv = document.createElement('div')
+  gameBoardAndKeyboardDiv.className = 'game-board-and-keyboard'
+  document.body.appendChild(gameBoardAndKeyboardDiv)
+  //make "game-board-Container" flex
+  let gameBoardContainerDiv = document.createElement('div')
+  gameBoardContainerDiv.className = 'game-board-Container'
+  gameBoardAndKeyboardDiv.appendChild(gameBoardContainerDiv)
+  //make "game-board" grid
+
   for (let i = 0; i < 6; i++) {
-    //make a new row
+    //make a new row (grid)
     for (let j = 0; j < 5; j++) {
-      //make 5 containers in the row
+      //make 5 containers in the row (flex)
     }
   }
 }
+initGameBoard()
