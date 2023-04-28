adca339996af5dfa87c510a1d101475b399196c6
Max Calabro <maxccalabro@gmail.com>
2023-04-28 15:27:50 -0400

    mostly changed stuff on my notepad but its been a minite since i commited so

---
+++ script.js
@@ -0,0 +1,13 @@
+import { WORDS } from './words.js'
+
+let solution = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(solution)
+
+const initGameBoard = () => {
+  for (let i = 0; i < 6; i++) {
+    //make a new row
+    for (let j = 0; j < 5; j++) {
+      //make 5 containers in the row
+    }
+  }
+}
